0cb9da312efb22fa2dc838d7b2b18d4a27a1f87e
CauaMata14 <caua.mata@iesb.edu.br>
2025-11-18 14:29:25 -0300

    Initial project setup with Express and MongoDB configuration

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./src/config/db');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
